452de76190e754663b7aa2ff8e47d21d3e22c9e0
rosydiazl <rosyosmediazl@gmail.com>
2021-09-18 14:28:25 -0400

    Add a fully functional delete button that makes a DELETE request. Also imported Image from Cloudinary and it is being displayed on the app

--- src/components/profile/ShowProfile.js
+++ src/components/profile/ShowProfile.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-tabs */
 import React, { Component } from 'react'
-import { indexUserProfile } from '../../api/profile'
+import { indexUserProfile, deleteProfile } from '../../api/profile'
 import { withRouter } from 'react-router-dom'
 import { Card, Button } from 'react-bootstrap'
 
@@ -24,55 +24,88 @@ class ShowProfile extends Component {
       .then(() =>
         msgAlert({
           heading: 'Success',
-          message: 'Here\'s the profiles',
+          message: 'Here\'s your profile',
           variant: 'success'
         })
       )
       .catch((err) =>
         msgAlert({
-          heading: 'Index failed :(',
-          message: 'Something went wrong: ' + err.message,
+          heading: 'Oh, no. Something went wrong!',
+          message: 'Error: ' + err.message,
           variant: 'danger'
         })
       )
   }
 
-  render () {
-    const cardContainerLayout = {
-      display: 'flex',
-      justifyContent: 'center',
-      flexFlow: 'row wrap'
-    }
-    const { userprofile } = this.state
-    // This is what prevents the "cannot read property map of undefined" or other similar errors because on the first render, `movies` state will be `null`
-    if (userprofile === null) {
-      return 'Loading...'
-    }
+	handleClick = (event) => {
+	  const { user, msgAlert, history } = this.props
+	  event.preventDefault()
+	  const profileDataId = event.target.getAttribute('data-id')
+	  // console.log('Data-id is', profileDataId)
+	  deleteProfile(profileDataId, user)
+	    .then(() =>
+	      msgAlert({
+	        heading: 'Deleted',
+	        message: 'Profile deleted successfully',
+	        variant: 'success'
+	      })
+	    )
+	    .then(() => history.push('/userprofile'))
+	    .then(() =>
+	      indexUserProfile(user).then((response) => {
+	        this.setState({
+	          userprofile: response.data.userprofile
+	        })
+	      })
+	    )
+	    .catch((err) =>
+	      msgAlert({
+	        heading: 'Deletion failed',
+	        message: 'Unable to delete your profile. Error: ' + err.message,
+	        variant: 'danger'
+	      })
+	    )
+	}
 
-    let profileJsx
-    console.log('This is the user profile', this.state.userprofile)
-    if (this.state.userprofile.length === 0) {
-      profileJsx = 'You do not have a profile yet. Create one!'
-    } else {
-      // I want movieJsx to be a bunch of li or Link or something with all my movies info in them
-      // .map gives us back a new array that we can display
-      profileJsx = this.state.userprofile.map((profile) => (
-        <Card key={profile.id} style={{ width: '18rem', marginTop: '40px' }}>
-          <Card.Body></Card.Body>
-          <Card.Body>
-            <Card.Title>{profile.name}</Card.Title>
-            <Card.Text>{profile.age}</Card.Text>
-            <Card.Text>{profile.breed}</Card.Text>
-            <Card.Text>{profile.bio}</Card.Text>
-            <Card.Text>{profile.image}</Card.Text>
-            <Button variant='danger'>Delete</Button>
-            <Button variant='dark'>Update</Button>
-          </Card.Body>
-        </Card>
-      ))
-    }
+	render () {
+	  const cardContainerLayout = {
+	    display: 'flex',
+	    justifyContent: 'center',
+	    flexFlow: 'row wrap'
+	  }
+	  const { userprofile } = this.state
+	  // This is what prevents the "cannot read property map of undefined" or other similar errors because on the first render, `movies` state will be `null`
+	  if (userprofile === null) {
+	    return 'Loading...'
+	  }
 
-    return <div style={cardContainerLayout}>{profileJsx}</div>
-  }
+	  let profileJsx
+	  // console.log('This is the user profile', this.state.userprofile)
+	  if (this.state.userprofile.length === 0) {
+	    profileJsx = 'You do not have a profile yet. Create one!'
+	  } else {
+	    // I want movieJsx to be a bunch of li or Link or something with all my movies info in them
+	    // .map gives us back a new array that we can display
+	    profileJsx = this.state.userprofile.map((profile) => (
+	      <Card key={profile.id} style={{ width: '18rem', marginTop: '40px' }}>
+	        <Card.Body>
+	          <Card.Title>{profile.name}</Card.Title>
+	          <Card.Text>{profile.age}</Card.Text>
+	          <Card.Text>{profile.breed}</Card.Text>
+	          <Card.Text>{profile.bio}</Card.Text>
+	          <Button variant='dark'>Update</Button>
+	          <Button
+	            data-id={profile.id}
+	            variant='danger'
+	            onClick={this.handleClick}>
+							Delete
+	          </Button>
+	        </Card.Body>
+	      </Card>
+	    ))
+	  }
+
+	  return <div style={cardContainerLayout}>{profileJsx}</div>
+	}
 }
 export default withRouter(ShowProfile)
