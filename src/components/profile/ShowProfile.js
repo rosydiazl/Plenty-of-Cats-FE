68883a6979b44edef54e4546af7c1697238c2c0a
rosydiazl <rosyosmediazl@gmail.com>
2021-09-18 21:17:22 -0400

    Add history push to user profile, id, update on the Update button so it redirects the user to where they can update their profile

--- src/components/profile/ShowProfile.js
+++ src/components/profile/ShowProfile.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import { indexUserProfile, deleteProfile } from '../../api/profile'
 import { withRouter } from 'react-router-dom'
 import { Card, Button } from 'react-bootstrap'
+import { Image } from 'cloudinary-react'
 
 class ShowProfile extends Component {
   constructor (props) {
@@ -23,8 +24,8 @@ class ShowProfile extends Component {
       })
       .then(() =>
         msgAlert({
-          heading: 'Success',
-          message: 'Here\'s your profile',
+          heading: 'Profile',
+          message: 'Here\'s your profile!',
           variant: 'success'
         })
       )
@@ -74,6 +75,7 @@ class ShowProfile extends Component {
 	    flexFlow: 'row wrap'
 	  }
 	  const { userprofile } = this.state
+	  const { history } = this.props
 	  // This is what prevents the "cannot read property map of undefined" or other similar errors because on the first render, `movies` state will be `null`
 	  if (userprofile === null) {
 	    return 'Loading...'
@@ -82,18 +84,29 @@ class ShowProfile extends Component {
 	  let profileJsx
 	  // console.log('This is the user profile', this.state.userprofile)
 	  if (this.state.userprofile.length === 0) {
-	    profileJsx = 'You do not have a profile yet. Create one!'
+	    profileJsx = 'You do not have a profile yet. Create one below!'
 	  } else {
 	    // I want movieJsx to be a bunch of li or Link or something with all my movies info in them
 	    // .map gives us back a new array that we can display
 	    profileJsx = this.state.userprofile.map((profile) => (
-	      <Card key={profile.id} style={{ width: '18rem', marginTop: '40px' }}>
+	      <Card
+	        key={profile.id}
+	        style={{ width: '18rem', marginTop: '40px' }}>
 	        <Card.Body>
 	          <Card.Title>{profile.name}</Card.Title>
+	          <Image
+	            style={{ width: 257 }}
+	            cloudName='doz8kotw7'
+	            publicId='https://res.cloudinary.com/doz8kotw7/image/upload/v1631985746/jqaomhzzofunvjn6jzt4.jpg'
+	          />
 	          <Card.Text>{profile.age}</Card.Text>
 	          <Card.Text>{profile.breed}</Card.Text>
 	          <Card.Text>{profile.bio}</Card.Text>
-	          <Button variant='dark'>Update</Button>
+	          <Button
+	            variant='dark'
+	            onClick={() => history.push(`/userprofile/${profile.id}/update`)}>
+							Update
+	          </Button>
 	          <Button
 	            data-id={profile.id}
 	            variant='danger'
