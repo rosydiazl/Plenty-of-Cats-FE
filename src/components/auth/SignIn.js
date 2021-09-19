769ff8d3a0b9419eae5bfe62732a66a6e202d318
rosydiazl <rosyosmediazl@gmail.com>
2021-09-19 11:41:15 -0400

    Capitalize the title of the form

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-tabs */
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
 import { signIn } from '../../api/auth'
 import { signInSuccess, signInFailure } from '../AutoDismissAlert/messages'
+// import { pic.webp } from  '../../../src/pic.webp'
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
@@ -53,31 +55,33 @@ render () {
   return (
     <div className='row'>
       <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-        <h3>Sign In</h3>
+        <h3>SIGN IN</h3>
         <Form onSubmit={this.onSignIn}>
           <Form.Group controlId='email'>
-            <Form.Label>Email address</Form.Label>
             <Form.Control
               required
               type='email'
               name='email'
               value={email}
-              placeholder='Enter email'
+              placeholder='Email address'
+              style={{ marginBottom: '20px' }}
               onChange={this.handleChange}
             />
           </Form.Group>
           <Form.Group controlId='password'>
-            <Form.Label>Password</Form.Label>
             <Form.Control
               required
               name='password'
               value={password}
               type='password'
               placeholder='Password'
+              style={{ marginBottom: '10px' }}
               onChange={this.handleChange}
             />
           </Form.Group>
-          <Button variant='primary' type='submit'>Submit</Button>
+          <Button variant='primary' type='submit'>
+						Submit
+          </Button>
         </Form>
       </div>
     </div>
