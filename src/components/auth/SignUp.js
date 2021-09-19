d3d18389b539fcf9091244f55d312fdb56ceb619
rosydiazl <rosyosmediazl@gmail.com>
2021-09-19 11:41:29 -0400

    Capitalize the title of the form Sign Up

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-tabs */
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
@@ -55,42 +56,44 @@ render () {
   return (
     <div className='row'>
       <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-        <h3>Sign Up</h3>
+        <h3>SIGN UP</h3>
         <Form onSubmit={this.onSignUp}>
           <Form.Group controlId='email'>
-            <Form.Label>Email address</Form.Label>
             <Form.Control
               required
               type='email'
               name='email'
               value={email}
-              placeholder='Enter email'
+              placeholder='Email address'
+              style={{ marginBottom: '20px' }}
               onChange={this.handleChange}
             />
           </Form.Group>
           <Form.Group controlId='password'>
-            <Form.Label>Password</Form.Label>
             <Form.Control
               required
               name='password'
               value={password}
               type='password'
               placeholder='Password'
+              style={{ marginBottom: '20px' }}
               onChange={this.handleChange}
             />
           </Form.Group>
           <Form.Group controlId='passwordConfirmation'>
-            <Form.Label>Password Confirmation</Form.Label>
             <Form.Control
               required
               name='passwordConfirmation'
               value={passwordConfirmation}
               type='password'
               placeholder='Confirm Password'
+              style={{ marginBottom: '10px' }}
               onChange={this.handleChange}
             />
           </Form.Group>
-          <Button variant='primary' type='submit'>Submit</Button>
+          <Button variant='primary' type='submit'>
+						Submit
+          </Button>
         </Form>
       </div>
     </div>
