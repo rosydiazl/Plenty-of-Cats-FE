51b1c61f919bd4d0dbad75df6817b50bcbf06c27
rosydiazl <rosyosmediazl@gmail.com>
2021-09-18 21:18:11 -0400

    Update the NavLink that is an alwaysOption to be /profiles instead of just /

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -30,7 +30,7 @@ const unauthenticatedOptions = (
 
 const alwaysOptions = (
   <Fragment>
-    <NavLink to='/' className='nav-link'>Home</NavLink>
+    <NavLink to='/profiles' className='nav-link'>Home</NavLink>
   </Fragment>
 )
 
